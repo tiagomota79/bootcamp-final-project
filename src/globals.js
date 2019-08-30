11d1f39b59efd7f0b890a1d17ff4cea108b893db
Tiago Mota <tiagomota@Tiagos-MacBook-Pro.local>
2019-08-30 10:26:33 -0400

    Globals

---
+++ src/globals.js
@@ -0,0 +1,9 @@
+export const colors = {
+  blue: '#1b98e0',
+  offWhite: '#fcfdff',
+  lightGrey: '#dbe1de',
+  darkBlue: '#1c6f9e',
+  darkGrey: '#8e9aaf',
+};
+
+export const boxShadow = `0px 5px 10px 5px rgba(0, 0, 0, 0.25)`;
